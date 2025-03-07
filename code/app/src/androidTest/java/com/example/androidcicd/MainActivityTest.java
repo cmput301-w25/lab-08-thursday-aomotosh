bb02d6cf66de48fd4a3c8229e5c1bd2a246f3292
aomotosh <aomotosh@ualberta.ca>
2025-03-07 01:58:22 -0700

    All testing Works

--- code/app/src/androidTest/java/com/example/androidcicd/MainActivityTest.java
+++ code/app/src/androidTest/java/com/example/androidcicd/MainActivityTest.java
@@ -102,8 +102,9 @@ public class MainActivityTest {
     }
 
     @Test
-    public void appShouldDisplayExistingMoviesOnLaunch() {
+    public void appShouldDisplayExistingMoviesOnLaunch() throws InterruptedException{
         // Check that the initial data is loaded
+        Thread.sleep(3000);
         onView(withText("Oppenheimer")).check(matches(isDisplayed()));
         onView(withText("Barbie")).check(matches(isDisplayed()));
 
@@ -128,7 +129,7 @@ public class MainActivityTest {
 
     @After
     public void tearDown() {
-        String projectId = "YOUR-PROJECT-ID";
+        String projectId = "lab8-af5b8";
         URL url = null;
         try {
             url = new URL("http://10.0.2.2:8080/emulator/v1/projects/" + projectId + "/databases/(default)/documents");
